fe0c4a607e5e46b1d43fd2a69d400a8e6a1d3880
lambocityyachtclub-png <lambocityyachtclub@gmail.com>
2026-06-18 03:01:12 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
+import { initScene, scene } from "./engine/scene.js";
+import { initCamera, camera } from "./engine/camera.js";
+import { initRenderer, renderer } from "./engine/renderer.js";
 
+import { initMovement, updateMovement } from "./systems/movement.js";
+import { initNPC, updateNPC } from "./systems/npc.js";
+import { initDriving, updateDriving } from "./systems/driving.js";
+import { initYacht } from "./systems/yacht.js";
+import { initAudio, updateAudio } from "./systems/audio.js";
+import { initDistricts, updateDistricts } from "./systems/districts.js";
+
+initScene();
+initCamera();
+initRenderer();
+
+initMovement();
+initNPC();
+initDriving();
+initYacht();
+initAudio();
+initDistricts();
+
+function animate(){
+requestAnimationFrame(animate);
+
+updateMovement();
+updateNPC();
+updateDriving();
+updateAudio();
+updateDistricts();
+
+renderer.render(scene, camera);
+}
+
+animate();
