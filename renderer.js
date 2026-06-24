107552ca6cc72d09a87883e5b8e46514bb622111
lambocityyachtclub-png <lambocityyachtclub@gmail.com>
2026-06-24 00:26:20 -0700

    Update renderer.js

--- renderer.js
+++ renderer.js
@@ -3,6 +3,8 @@ import * as THREE from "https://unpkg.com/three@0.160.0/build/three.module.js";
 export const renderer = new THREE.WebGLRenderer({ antialias: true });
 
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setClearColor(0x000000);
 
 document.body.style.margin = "0";
 document.body.appendChild(renderer.domElement);
