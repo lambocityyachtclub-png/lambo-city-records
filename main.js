9fdccbc79c565799cc117af06003f1d5dd9ec150
lambocityyachtclub-png <lambocityyachtclub@gmail.com>
2026-06-26 00:44:31 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+console.log("MAIN JS LOADED");
+console.log("ENGINE:", engine);
+
 import * as THREE from "https://unpkg.com/three@0.160.0/build/three.module.js";
 import { engine } from "./engine.js";
 
