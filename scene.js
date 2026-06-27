a61c4a1e5b4f52f483749c41674ae99d240511f4
lambocityyachtclub-png <lambocityyachtclub@gmail.com>
2026-06-27 01:51:17 -0700

    Update scene.js

--- scene.js
+++ scene.js
@@ -1,11 +1,15 @@
 import * as THREE from "https://unpkg.com/three@0.160.0/build/three.module.js";
 import { engine } from "./engine.js";
 
-/* =========================================================
-   🌍 ENGINE WORLD INITIALIZATION
-========================================================= */
-
 engine.scene = new THREE.Scene();
 engine.world = new THREE.Group();
 
 engine.scene.add(engine.world);
+
+/* =========================================================
+   🌌 GLOBAL VISIBILITY SETUP
+========================================================= */
+
+engine.scene.fog = new THREE.Fog(0x0a0a0a, 200, 2000);
+
+engine.scene.background = new THREE.Color(0x05070a);
