2a7d3a2800b0a60667b24e622403bed5cc2575db
lambocityyachtclub-png <lambocityyachtclub@gmail.com>
2026-06-27 05:35:33 +0000

    Update input.js

--- input.js
+++ input.js
@@ -1,1 +1,9 @@
+import { engine } from "./engine.js";
 
+window.addEventListener("keydown", (e) => {
+  engine.keys[e.code] = true;
+});
+
+window.addEventListener("keyup", (e) => {
+  engine.keys[e.code] = false;
+});
