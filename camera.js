317edb3be1327cffa4c27427aa856afea34ca62f
lambocityyachtclub-png <lambocityyachtclub@gmail.com>
2026-06-27 02:11:41 -0700

    Update camera.js

--- camera.js
+++ camera.js
@@ -1,10 +1,11 @@
 import * as THREE from "https://unpkg.com/three@0.160.0/build/three.module.js";
+import { engine } from "./engine.js";
 
-export const camera = new THREE.PerspectiveCamera(
+engine.camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
   0.1,
-  2000
+  20000
 );
 
 engine.camera.position.set(0, 120, 600);
