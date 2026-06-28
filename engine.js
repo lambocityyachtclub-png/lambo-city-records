14eac10a96a332a1fa6779f36c28c8165fdba9fd
lambocityyachtclub-png <lambocityyachtclub@gmail.com>
2026-06-28 15:12:42 -0700

    Update engine.js

--- engine.js
+++ engine.js
@@ -1,46 +1,25 @@
-import * as THREE from "https://unpkg.com/three@0.160.0/build/three.module.js";
+start() {
+  const loop = () => {
+    requestAnimationFrame(loop);
 
-export const engine = {
-  scene: null,
-  camera: null,
-  renderer: null,
+    this.delta = this.clock.getDelta();
+    this.elapsed += this.delta;
 
-  world: new THREE.Group(),
+    for (const sys of this.systems) {
+      sys(this);
+    }
 
-  player: null,
+    if (this.updateCamera) {
+      this.updateCamera(this);
+    }
 
-  keys: {},
+    // 🚨 DEBUG FORCE LOG
+    if (!this.scene) console.error("NO SCENE");
+    if (!this.camera) console.error("NO CAMERA");
+    if (!this.renderer) console.error("NO RENDERER");
 
-  systems: [],
+    this.renderer.render(this.scene, this.camera);
+  };
 
-  clock: new THREE.Clock(),
-  delta: 0,
-  elapsed: 0,
-
-  registerSystem(fn) {
-    this.systems.push(fn);
-  },
-
-  start() {
-    const loop = () => {
-      requestAnimationFrame(loop);
-
-      this.delta = this.clock.getDelta();
-      this.elapsed += this.delta;
-
-      for (const sys of this.systems) {
-        sys(this);
-      }
-
-      if (this.updateCamera) {
-        this.updateCamera(this);
-      }
-
-      if (this.renderer && this.scene && this.camera) {
-        this.renderer.render(this.scene, this.camera);
-      }
-    };
-
-    loop();
-  }
-};
+  loop();
+}
