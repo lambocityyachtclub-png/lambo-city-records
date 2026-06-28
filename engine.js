cc3c7a4735fc9910902e8ef0db44c505e3e1caae
lambocityyachtclub-png <lambocityyachtclub@gmail.com>
2026-06-27 21:47:35 -0700

    Update engine.js

--- engine.js
+++ engine.js
@@ -1,7 +1,6 @@
 import * as THREE from "https://unpkg.com/three@0.160.0/build/three.module.js";
 
 export const engine = {
-
   scene: new THREE.Scene(),
   camera: null,
   renderer: null,
@@ -11,35 +10,23 @@ export const engine = {
   player: null,
   keys: {},
 
-  clock: new THREE.Clock(),
-
   systems: [],
-
-  playerVelocity: new THREE.Vector3(),
-
-  state: {
-    phase: "DOCK"
-  },
 
   registerSystem(fn) {
     this.systems.push(fn);
   },
 
   start() {
-
     const loop = () => {
-
       requestAnimationFrame(loop);
 
-      // run systems
       for (const sys of this.systems) {
         sys();
       }
 
-      // SAFETY RENDER CHECK
-      if (!this.renderer || !this.scene || !this.camera) return;
-
-      this.renderer.render(this.scene, this.camera);
+      if (this.renderer && this.scene && this.camera) {
+        this.renderer.render(this.scene, this.camera);
+      }
     };
 
     loop();
