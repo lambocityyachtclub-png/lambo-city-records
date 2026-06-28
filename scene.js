8a756b7d8cce8cf19dd16e52bff70111c053f576
lambocityyachtclub-png <lambocityyachtclub@gmail.com>
2026-06-28 15:39:32 -0700

    Update scene.js

--- scene.js
+++ scene.js
@@ -1,19 +1,12 @@
 import * as THREE from "https://unpkg.com/three@0.160.0/build/three.module.js";
 import { engine } from "./engine.js";
 
+/* FORCE SCENE */
 engine.scene = new THREE.Scene();
 
-/* BACKGROUND */
-engine.scene.background = new THREE.Color(0x87b5ff);
-engine.scene.fog = new THREE.Fog(0x87b5ff, 50, 200);
-
-/* LIGHTING */
-const ambient = new THREE.AmbientLight(0xffffff, 1);
-engine.scene.add(ambient);
-
-const sun = new THREE.DirectionalLight(0xffffff, 2);
-sun.position.set(50, 100, 50);
-engine.scene.add(sun);
+/* LIGHT */
+engine.scene.add(new THREE.AmbientLight(0xffffff, 1));
+engine.scene.add(new THREE.DirectionalLight(0xffffff, 2));
 
 /* ATTACH WORLD ROOT */
 engine.scene.add(engine.world);
