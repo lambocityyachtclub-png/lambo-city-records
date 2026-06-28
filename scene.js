c82b83014b871272b8eb94d514d8f8df9685faed
lambocityyachtclub-png <lambocityyachtclub@gmail.com>
2026-06-28 14:57:49 -0700

    Update scene.js

--- scene.js
+++ scene.js
@@ -2,7 +2,7 @@ import * as THREE from "https://unpkg.com/three@0.160.0/build/three.module.js";
 import { engine } from "./engine.js";
 
 /* =========================
-   SCENE BASE
+   SCENE BASE SETUP
 ========================= */
 
 engine.scene.background = new THREE.Color(0x0a1420);
@@ -13,11 +13,11 @@ engine.scene.fog = new THREE.FogExp2(0x0a1420, 0.002);
    LIGHTING SYSTEM
 ========================= */
 
-// Ambient base
+// Ambient light
 const ambient = new THREE.AmbientLight(0xffffff, 0.6);
 engine.scene.add(ambient);
 
-// Sun light (key cinematic light)
+// Sunlight (key light)
 const sun = new THREE.DirectionalLight(0xffddaa, 1.6);
 sun.position.set(100, 200, 100);
 sun.castShadow = true;
@@ -27,7 +27,6 @@ sun.shadow.mapSize.height = 2048;
 
 sun.shadow.camera.near = 0.5;
 sun.shadow.camera.far = 1000;
-
 sun.shadow.camera.left = -200;
 sun.shadow.camera.right = 200;
 sun.shadow.camera.top = 200;
@@ -42,7 +41,7 @@ fill.position.set(-100, 80, -100);
 engine.scene.add(fill);
 
 /* =========================
-   WORLD ROOT
+   WORLD ROOT ATTACH
 ========================= */
 
 engine.scene.add(engine.world);
