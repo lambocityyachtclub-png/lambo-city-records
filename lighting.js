c0891455c9d8fc0960128e32486f8c8829ba2db7
lambocityyachtclub-png <lambocityyachtclub@gmail.com>
2026-06-29 00:42:03 -0700

    Update lighting.js

--- lighting.js
+++ lighting.js
@@ -2,14 +2,12 @@ import * as THREE from "https://unpkg.com/three@0.160.0/build/three.module.js";
 
 export default {
   init(scene) {
-    const ambient = new THREE.AmbientLight(0xffffff, 0.5);
+    const ambient = new THREE.AmbientLight(0xffffff, 0.4);
     scene.add(ambient);
 
-    const sun = new THREE.DirectionalLight(0xffcc88, 1.5);
-    sun.position.set(20, 30, 10);
+    const sun = new THREE.DirectionalLight(0xffcc88, 1.8);
+    sun.position.set(30, 40, 20);
 
     scene.add(sun);
-  },
-
-  update() {}
+  }
 };
