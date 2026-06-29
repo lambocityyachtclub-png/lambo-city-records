96f4d585d75dfa2a1ce4d8b9a9f6168d43732994
lambocityyachtclub-png <lambocityyachtclub@gmail.com>
2026-06-29 00:21:13 -0700

    Update dock.js

--- dock.js
+++ dock.js
@@ -1,37 +1,14 @@
 import * as THREE from "https://unpkg.com/three@0.160.0/build/three.module.js";
 
-let dockGroup;
-
 export default {
   init(scene) {
-    dockGroup = new THREE.Group();
-
     const dock = new THREE.Mesh(
-      new THREE.BoxGeometry(20, 1, 6),
-      new THREE.MeshStandardMaterial({
-        color: 0x5a3a1a,
-        roughness: 1
-      })
+      new THREE.BoxGeometry(30, 1, 8),
+      new THREE.MeshStandardMaterial({ color: 0x3b2a1a })
     );
 
     dock.position.y = 0;
-    dockGroup.add(dock);
-
-    const postGeo = new THREE.CylinderGeometry(0.2, 0.2, 2, 8);
-    const postMat = new THREE.MeshStandardMaterial({ color: 0xffffff });
-
-    for (let i = -8; i <= 8; i += 4) {
-      const p1 = new THREE.Mesh(postGeo, postMat);
-      p1.position.set(i, 1, 3);
-
-      const p2 = new THREE.Mesh(postGeo, postMat);
-      p2.position.set(i, 1, -3);
-
-      dockGroup.add(p1);
-      dockGroup.add(p2);
-    }
-
-    scene.add(dockGroup);
+    scene.add(dock);
   },
 
   update() {}
