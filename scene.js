9cbf4670a5886c73537b4aa59911b9297976b66b
lambocityyachtclub-png <lambocityyachtclub@gmail.com>
2026-06-29 01:49:36 -0700

    Update scene.js

--- scene.js
+++ scene.js
@@ -22,3 +22,20 @@ export default {
     return scene;
   }
 };
+
+import * as THREE from "https://unpkg.com/three@0.160.0/build/three.module.js";
+
+export default {
+  init(scene) {
+    const marker = new THREE.Mesh(
+      new THREE.BoxGeometry(3, 3, 3),
+      new THREE.MeshBasicMaterial({ color: 0xffff00 })
+    );
+
+    marker.position.set(0, 10, 0);
+
+    scene.add(marker);
+
+    console.log("CANARY ADDED");
+  }
+};
