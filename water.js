999839f7b5e7bb8ea7bdcf3ed2abcf54521db731
lambocityyachtclub-png <lambocityyachtclub@gmail.com>
2026-06-29 00:19:54 -0700

    Update water.js

--- water.js
+++ water.js
@@ -1,17 +1,16 @@
 import * as THREE from "https://unpkg.com/three@0.160.0/build/three.module.js";
 
-let water;
-
 export default {
   init(scene) {
-    const geometry = new THREE.PlaneGeometry(200, 200);
+    const geometry = new THREE.PlaneGeometry(500, 500);
+
     const material = new THREE.MeshStandardMaterial({
-      color: 0x003344,
-      roughness: 0.2,
-      metalness: 0.6
+      color: 0x006680,
+      roughness: 0.25,
+      metalness: 0.4
     });
 
-    water = new THREE.Mesh(geometry, material);
+    const water = new THREE.Mesh(geometry, material);
 
     water.rotation.x = -Math.PI / 2;
     water.position.y = -1;
@@ -19,7 +18,5 @@ export default {
     scene.add(water);
   },
 
-  update() {
-    // later: wave animation
-  }
+  update() {}
 };
