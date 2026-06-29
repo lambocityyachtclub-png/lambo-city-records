28af9076712f3d968ad4705209bd63cf0808f616
lambocityyachtclub-png <lambocityyachtclub@gmail.com>
2026-06-29 01:20:09 -0700

    Update engine.js

--- engine.js
+++ engine.js
@@ -1,6 +1,10 @@
 export default class Engine {
   constructor() {
     this.systems = {};
+    this.lastTime = 0;
+
+    // shared world state (CRITICAL)
+    this.context = {};
   }
 
   registerSystems(systems) {
@@ -8,29 +12,46 @@ export default class Engine {
   }
 
   init() {
+    // CORE SYSTEMS
     this.scene = this.systems.scene?.init?.();
     this.camera = this.systems.camera?.init?.();
     this.renderer = this.systems.renderer?.init?.();
 
+    // STORE IN CONTEXT
+    this.context.scene = this.scene;
+    this.context.camera = this.camera;
+    this.context.renderer = this.renderer;
+
+    // WORLD INIT (pass scene)
     this.systems.world?.init?.(this.scene);
     this.systems.water?.init?.(this.scene);
     this.systems.dock?.init?.(this.scene);
     this.systems.sky?.init?.(this.scene);
     this.systems.lighting?.init?.(this.scene);
 
+    // PLAYER SYSTEM INIT (IMPORTANT)
+    this.systems.input?.init?.();
+    this.systems.player?.init?.(this.scene);
+
     this.loop();
   }
 
-  loop = () => {
+  loop = (time = 0) => {
     requestAnimationFrame(this.loop);
 
-    this.update();
+    const delta = (time - this.lastTime) / 1000;
+    this.lastTime = time;
+
+    this.update(delta);
     this.render();
   };
 
-  update() {
-    this.systems.player?.update?.();
-    this.systems.input?.update?.();
+  update(delta) {
+    // PASS CONTEXT TO EVERYTHING
+    this.context.delta = delta;
+
+    this.systems.input?.update?.(delta, this.context);
+    this.systems.player?.update?.(delta, this.context);
   }
 
   render() {
