8c95a5232c0a369c69080af08dfb19497257f8cc
lambocityyachtclub-png <lambocityyachtclub@gmail.com>
2026-06-29 02:49:29 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,37 +1,30 @@
 import Engine from "./engine.js";
-
 import Scene from "./scene.js";
 import Camera from "./camera.js";
 import Renderer from "./renderer.js";
-
 import Dock from "./dock.js";
 import Water from "./water.js";
 import Sky from "./sky.js";
 import Lighting from "./lighting.js";
-
 import Palms from "./palms.js";
 import Yacht from "./yacht.js";
-
 import Input from "./input.js";
 import Player from "./player.js";
+import HUD from "./hud.js";
 
 const engine = new Engine();
-
 engine.registerSystems({
   scene: Scene,
   camera: Camera,
   renderer: Renderer,
-
   input: Input,
   player: Player,
-
   dock: Dock,
   water: Water,
   sky: Sky,
   lighting: Lighting,
-
   palms: Palms,
-  yacht: Yacht
+  yacht: Yacht,
+  hud: HUD,
 });
-
 engine.init();
