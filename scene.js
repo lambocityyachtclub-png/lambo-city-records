884c7a4d56b2a4dfd0b48e32ed46cf2797653959
lambocityyachtclub-png <lambocityyachtclub@gmail.com>
2026-06-29 00:31:27 -0700

    Update scene.js

--- scene.js
+++ scene.js
@@ -5,18 +5,17 @@ let scene;
 export default {
   init() {
     scene = new THREE.Scene();
-
-    // background (so we KNOW render is working)
     scene.background = new THREE.Color(0x003344);
 
-    // TEST OBJECT (must appear if everything works)
     const cube = new THREE.Mesh(
       new THREE.BoxGeometry(2, 2, 2),
-      new THREE.MeshBasicMaterial({ color: 0xff0000 })
+      new THREE.MeshStandardMaterial({ color: 0xff0000 })
     );
 
     cube.position.set(0, 1, 0);
     scene.add(cube);
+
+    return scene; // 🔥 CRITICAL
   },
 
   getScene() {
