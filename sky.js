28b64a6adbfae453976d3196bda85e9a7a5cead4
lambocityyachtclub-png <lambocityyachtclub@gmail.com>
2026-06-29 00:21:53 -0700

    Create sky.js

---
+++ sky.js
@@ -0,0 +1,9 @@
+import * as THREE from "https://unpkg.com/three@0.160.0/build/three.module.js";
+
+export default {
+  init(scene) {
+    scene.background = new THREE.Color(0x87ceeb);
+  },
+
+  update() {}
+};
