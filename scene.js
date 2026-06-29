9b49ae09030cb02629c7cda8e907b37c6229dd1d
lambocityyachtclub-png <lambocityyachtclub@gmail.com>
2026-06-28 22:51:04 -0700

    Update scene.js

--- scene.js
+++ scene.js
@@ -1,3 +1,27 @@
-import { engine } from "./engine.js";
+import * as THREE from "https://unpkg.com/three@0.160.0/build/three.module.js";
 
-/* DO NOT CREATE DUPLICATE SCENES */
+let scene;
+
+export default {
+  init() {
+    scene = new THREE.Scene();
+
+    // 🔥 FORCE BACKGROUND COLOR
+    scene.background = new THREE.Color(0x003344);
+
+    // 🔥 FORCE VISIBLE OBJECT
+    const cube = new THREE.Mesh(
+      new THREE.BoxGeometry(2, 2, 2),
+      new THREE.MeshBasicMaterial({ color: 0xff0000 })
+    );
+
+    cube.position.set(0, 1, 0);
+    scene.add(cube);
+
+    this._scene = scene;
+  },
+
+  getScene() {
+    return this._scene;
+  }
+};
