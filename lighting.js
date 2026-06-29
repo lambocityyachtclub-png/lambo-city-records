d2e3def98c31effeddb5e64562c8eeac09e5356c
lambocityyachtclub-png <lambocityyachtclub@gmail.com>
2026-06-29 00:22:56 -0700

    Update lighting.js

--- lighting.js
+++ lighting.js
@@ -2,18 +2,14 @@ import * as THREE from "https://unpkg.com/three@0.160.0/build/three.module.js";
 
 export default {
   init(scene) {
-    // soft ambient base light
-    const ambient = new THREE.AmbientLight(0xffffff, 0.4);
+    const ambient = new THREE.AmbientLight(0xffffff, 0.5);
     scene.add(ambient);
 
-    // main cinematic “sun” light
-    const sun = new THREE.DirectionalLight(0xffcc88, 1.2);
-    sun.position.set(10, 20, 10);
+    const sun = new THREE.DirectionalLight(0xffcc88, 1.5);
+    sun.position.set(20, 30, 10);
 
     scene.add(sun);
   },
 
-  update() {
-    // later we can animate sun angle for day/night cycle
-  }
+  update() {}
 };
