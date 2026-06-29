f50dcac3c78d4eae28f0abb2fba6ee45cc8ba515
lambocityyachtclub-png <lambocityyachtclub@gmail.com>
2026-06-28 22:55:49 -0700

    Update camera.js

--- camera.js
+++ camera.js
@@ -1,20 +1,25 @@
 import * as THREE from "https://unpkg.com/three@0.160.0/build/three.module.js";
-import { engine } from "./engine.js";
 
-export function initCameraSystem() {
-  const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    2000
-  );
+let camera;
 
-  camera.position.set(0, 10, 20);
-  camera.lookAt(0, 0, 0);
+export default {
+  init() {
+    camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      2000
+    );
 
-  engine.camera = camera;
-
-  engine.updateCamera = function () {
+    camera.position.set(0, 10, 20);
     camera.lookAt(0, 0, 0);
-  };
-}
+  },
+
+  update() {
+    // optional future camera logic
+  },
+
+  getCamera() {
+    return camera;
+  }
+};
