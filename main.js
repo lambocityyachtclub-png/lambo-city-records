05052498ead1ee972ef18080ff867a4c4982b042
lambocityyachtclub-png <lambocityyachtclub@gmail.com>
2026-07-19 17:36:26 -0700

    Update main.js

--- main.js
+++ main.js
@@ -24,7 +24,6 @@ import StageBanner from "./stageBanner.js";
 import AmbientMusic from "./ambientMusic.js";
 import StageAudioZone from "./stageAudioZone.js";
 import Collision from "./collision.js";
-import StageSpotlights from "./stageSpotlights.js";
 const engine = new Engine();
 engine.registerSystems({
   scene:         Scene,
@@ -49,7 +48,6 @@ engine.registerSystems({
   ambientMusic:  AmbientMusic,
   stageAudioZone: StageAudioZone,
   collision:     Collision,
-  stageSpotlights: StageSpotlights,
   input:         Input,
   player:        Player,
   hud:           HUD,
