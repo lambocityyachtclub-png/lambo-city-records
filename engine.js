1a8e3b841a2ff920d705e172301082170956acb0
lambocityyachtclub-png <lambocityyachtclub@gmail.com>
2026-07-23 01:15:31 -0700

    Initialize stageCrowdPulse system in engine

--- engine.js
+++ engine.js
@@ -37,6 +37,7 @@ export default class Engine {
     this.systems.stageSpotlights?.init?.(this.scene);
     this.systems.stageLasers?.init?.(this.scene);
     this.systems.stageSmoke?.init?.(this.scene);
+    this.systems.stageCrowdPulse?.init?.(this.scene);
     this.systems.input?.init?.();
     this.systems.player?.init?.(this.scene);
     this.systems.hud?.init?.();
