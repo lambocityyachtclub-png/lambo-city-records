f5b492ad29603123481b1460f5766ecb10b4969a
lambocityyachtclub-png <lambocityyachtclub@gmail.com>
2026-07-23 01:29:15 -0700

    Update engine.js

--- engine.js
+++ engine.js
@@ -38,6 +38,7 @@ export default class Engine {
     this.systems.stageLasers?.init?.(this.scene);
     this.systems.stageSmoke?.init?.(this.scene);
     this.systems.stageCrowdPulse?.init?.(this.scene);
+    this.systems.yachtLuxuryDetails?.init?.(this.scene);
     this.systems.input?.init?.();
     this.systems.player?.init?.(this.scene);
     this.systems.hud?.init?.();
