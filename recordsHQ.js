ccfef1b161092cfa42afacf92d3580f5255a93f4
lambocityyachtclub-png <lambocityyachtclub@gmail.com>
2026-07-29 18:39:21 -0700

    Update recordsHQ.js

--- recordsHQ.js
+++ recordsHQ.js
@@ -7,7 +7,7 @@
 
 import * as THREE from "https://unpkg.com/three@0.160.0/build/three.module.js";
 
-const HQ_POSITION = { x: 28, z: 28 };
+const HQ_POSITION = { x: 28, z: 22 };
 
 function createSignTexture() {
   const w = 1024, h = 512;
