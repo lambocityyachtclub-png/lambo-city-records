da6b9f8c6df73e91d251425fc3f85f0a9463d77f
lambocityyachtclub-png <lambocityyachtclub@gmail.com>
2026-08-14 21:05:05 -0700

    Add stageArchitecture to engine.js initialization

--- engine.js
+++ engine.js
@@ -29,6 +29,7 @@ export default class Engine {
     this.systems.dockCore?.init?.(this.scene);
     this.systems.marina?.init?.(this.scene);
     this.systems.stageVideo?.init?.(this.scene);
+    stageArchitecture: StageArchitecture,
     this.systems.stageScreenMedia?.init?.(this.scene);
     this.systems.stageBanner?.init?.(this.scene);
     this.systems.ambientMusic?.init?.();
