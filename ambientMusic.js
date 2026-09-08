e160a2427c28e253787f460d3a1f698f7300fd92
lambocityyachtclub-png <lambocityyachtclub@gmail.com>
2026-09-07 23:50:13 -0700

    Fix artist name from 'H.E.R.O' to 'HERO'

--- ambientMusic.js
+++ ambientMusic.js
@@ -14,12 +14,12 @@ const PLAYLIST = [
   },
   {
     title: "Bounce To The Oz",
-    artist: "H.E.R.O",
+    artist: "HERO",
     url: "https://res.cloudinary.com/z99sdnqv/video/upload/v1788849538/Bounce_To_The_Oz_3_dyjluf.mp3",
   },
   {
     title: "Everybody Get Up",
-    artist: "H.E.R.O",
+    artist: "HERO",
     url: "https://res.cloudinary.com/z99sdnqv/video/upload/v1788849476/H.E.R.O_Everybody_Get_Up_zn8li0.mp3",
   },
   // { title: "Next Song", artist: "Hero", url: "https://..." },
